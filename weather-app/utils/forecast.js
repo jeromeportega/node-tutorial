d51e68d98e7e738bf06d17531ca6886a18d2e82a
Jerome Ortega <jeromeortega@Jeromes-MacBook-Pro-2.local>
2019-03-26 06:02:12 -0500

    weather app asynchronous with destructuring

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -3,16 +3,15 @@ const request = require('request');
 const forecast = (latitude, longitude, callback) => {
     const url = `https://api.darksky.net/forecast/2d10c24084c9087b75fa2324392255d1/${latitude},${longitude}`;
 
-    request({ url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather services.');
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location, please try another search.', undefined);
         } else {
-            console.log(response.body);
             callback(undefined, {
-                temperature: response.body.currently.temperature,
-                summary: response.body.daily.summary,
+                temperature: body.currently.temperature,
+                summary: body.daily.summary,
             });
         }
     });
