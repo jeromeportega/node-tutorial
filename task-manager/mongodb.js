4bbe7d01ac543687725a933f981f518f661a1ff2
Jerome Ortega <jeromeortega@Jeromes-MacBook-Pro-2.local>
2019-04-03 06:47:55 -0500

    fiddling around with CRUD in mongodb

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -5,10 +5,6 @@ const { MongoClient, ObjectID } = require('mongodb');
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
-const id = new ObjectID();
-
-console.log(id);
-
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database.');
@@ -16,52 +12,17 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     
     const db = client.db(databaseName);
 
-    // db.collection('users').insertOne({
-    //     name: 'Jerry',
-    //     age: 18
-    // }, (error, results) => {
-    //     if (error) {
-    //         return console.log('Unable to insert user!');
-    //     }
+    const updatePromise = db.collection('users').updateOne({
+        _id: new ObjectID("5ca421e575c95df141d7e189"),
+    }, {
+        $set: {
+            name: 'Mike'
+        },
+    });
 
-    //     console.log(results.ops);
-    // });
-
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Mary',
-    //         age: 23,
-    //     },
-    //     {
-    //         name: 'Jackson',
-    //         age: 30,
-    //     },
-    // ], (error, results) => {
-    //     if (error) {
-    //         console.log('Unable to insert documents!');
-    //     }
-
-    //     console.log(results.ops);
-    // });
-
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description: 'Take out the trash.',
-    //         completed: false,
-    //     },
-    //     {
-    //         description: 'Wash the dishes.',
-    //         completed: true,
-    //     },
-    //     {
-    //         description: 'Mow the Lawn',
-    //         completed: false,
-    //     },
-    // ], (error, results) => {
-    //     if (error) {
-    //         console.log('Unable to insert documents.');
-    //     }
-
-    //     console.log(results.ops);
-    // });
+    updatePromise.then((result) => {
+        console.log(result);
+    }).catch((error) => {
+        console.log(error);
+    });
 });
