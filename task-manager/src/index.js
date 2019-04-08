4dd67a733c9a30745bdd895c86aeda172f154844
Jerome Ortega <jeromeortega@Jeromes-MacBook-Pro-2.local>
2019-04-07 20:05:59 -0500

    adjusted adding users and tasks to allow middleware, plus hashing passwords with bcrypt

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,10 +7,9 @@ require('./db/mongoose');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 
+app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
-
-app.use(express.json());
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}.`);
