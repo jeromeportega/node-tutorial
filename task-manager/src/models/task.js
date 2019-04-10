d5086cbd5c1fd9a34b56aea1a9136440250261be
Jerome Ortega <jeromeortega@Jeromes-MacBook-Pro-2.local>
2019-04-10 06:47:42 -0500

    Cascading delete of user information

--- task-manager/src/models/task.js
+++ task-manager/src/models/task.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const Task = mongoose.model('Task', {
+const taskSchema = new mongoose.Schema({
     description: {
         type: String,
         required: true,
@@ -16,5 +16,7 @@ const Task = mongoose.model('Task', {
         ref: 'User',
     },
  });
+
+ const Task = mongoose.model('Task', taskSchema);
 
  module.exports = Task;
--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('./task');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -105,6 +106,12 @@ userSchema.pre('save', async function(next) {
         user.password = await bcrypt.hash(user.password, 8);
     }
 
+    next();
+});
+
+userSchema.pre('remove', async function(next) {
+    const user = this;
+    await Task.deleteMany({ owner: user._id });
     next();
 });
 
