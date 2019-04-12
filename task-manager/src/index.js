6bf71c37e428589e775b172fdb955d477f5c12ff
Jerome Ortega <jeromeortega@Jeromes-MacBook-Pro-2.local>
2019-04-12 06:44:29 -0500

    installed multer for files and added some comments

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -2,15 +2,20 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Bring in mongoose DB
 require('./db/mongoose');
 
+// Import Routes
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 
 app.use(express.json());
+
+// Use routes in the Express application
 app.use(userRouter);
 app.use(taskRouter);
 
+// Start the server.
 app.listen(port, () => {
     console.log(`Server is up on port ${port}.`);
 });
