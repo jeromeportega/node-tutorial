6e656580af846ddd3bcb3bb480e0ac31269750c9
Jerome Ortega <jeromeportega@gmail.com>
2019-06-06 05:59:23 -0500

    installed and hooked up socketio

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,17 +1,17 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = socketio(server);
 
+const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>');
-});
-
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+server.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
